2fd3b38beec3d40c4af4c984e233b49cdf2153b3
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-11 14:53:02 +0300

    Add landing page content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,21 @@
-function Header() {
-  return (
-    <div>Header</div>
-  )
+import Logo from "./Logo";
+
+const Header: React.FC = () => {
+    return (
+        <div id="header">
+            <div id="header-content">
+                <div id="header-logo">
+                    <Logo />
+                </div>
+
+                <div className="auth-btns">
+                    <button className="btn btn-primary">Login</button>
+                    <button className="btn btn-secondary">Sign Up</button>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Header
