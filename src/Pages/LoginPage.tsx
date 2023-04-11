149ba7db2b8615930d8f6897b75baf29e8308c54
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-11 16:02:16 +0300

    Create Login form

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,7 +1,14 @@
+import LoginContent from "../components/LoginContent"
+import LoginForm from "../components/LoginForm"
+
 function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <div id="login-page">
+            <LoginContent />
+            <LoginForm />
+
+        </div>
+    )
 }
 
 export default LoginPage
---
+++ src/components/LoginContent.tsx
@@ -0,0 +1,7 @@
+function LoginContent() {
+  return (
+    <div>LoginContext</div>
+  )
+}
+
+export default LoginContent
