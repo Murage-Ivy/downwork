ef2b93dde4ab7d233bce7dae59214e80b99cdd20
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-11 17:03:59 +0300

    Create and design login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,37 @@
+import { Link } from "react-router-dom"
 
 function LoginForm() {
-  return (
-    <div id="login-form">LoginForm</div>
-  )
+    return (
+        <div id="login-form">
+            <div className="form">
+                <h3>
+                    Sign in
+                </h3>
+
+                <p>
+                    Welcome back John Doe!
+                </p>
+            </div>
+            <form id="form">
+                <div className="form-group">
+                  
+                    <input type="email" name="email" id="email" placeholder="email" />
+                </div>
+                <div className="form-group">
+               
+                    <input type="password" name="password" id="password" placeholder="password" />
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-login">Login</button>
+                </div>
+                <div className="footer">
+                    <p>
+                        Don't have an account? <Link to="#">Sign up</Link>
+                    </p>
+                </div>
+            </form >
+        </div >
+    )
 }
 
 export default LoginForm
