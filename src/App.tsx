2b1c0e3fa22af59a209df93f395cb258f75d2b88
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-11 14:54:27 +0300

    Add Routing for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage";
 
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
 
     </div>
   );
