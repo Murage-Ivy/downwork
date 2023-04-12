5a5c769dd8ae00ed1fcb97dcea3007674de4639d
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-12 17:13:04 +0300

    Design and creat sign up form

--- src/Pages/SignUpPage.tsx
+++ src/Pages/SignUpPage.tsx
@@ -1,6 +1,17 @@
+import Logo from "../components/Logo"
+import SignUp from "../components/SignupForm"
+
 function SignUpPage() {
   return (
-    <div>SignUpPage</div>
+    <div id="signup-page">
+      <div className="float-right">
+        <Logo />
+      </div>
+      <div id="signup-form">
+        <SignUp />
+        <img src="../assets/images/countryside-woman-holding-plant-leaves-removebg-preview.png" alt="farmer" />
+      </div>
+    </div>
   )
 }
 
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom"
 
 const SignUp: React.FC = () => {
     return (
-        <div>
+        <div id="sign-form">
+            <div id="sign-form-content">
+
+                <form id="form-signup">
+                    <h1>SIGN UP</h1>
+                    <div className="sign-form-group">
+                        <input type="email" name="email" id="email" placeholder="email" autoComplete="email" />
+                        <input type="username" name="username" id="username" placeholder="username" autoComplete="username" />
+                    </div>
+                    <div className="sign-form-group">
+                        <input type="password" name="password" id="password" placeholder="password" autoComplete="current-password" />
+                        <input type="password" name="confirm-password" id="confirm-password" placeholder="confirm password" autoComplete="current confirm_password" />
+                    </div>
+                    <div className="sign-form-group">
+                        <input type="file" name="image" id="image" />
+                    </div>
+                    <div className="sign-form-group">
+                        <button id="sign-up-btn" type="submit">Sign Up</button>
+                    </div>
+                    <div className="sign-footer">
+                        <p>Already have an account? <Link to="/login">Login</Link></p>
+                    </div>
+                </form>
+            </div>
         </div>
 
     )
