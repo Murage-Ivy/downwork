2b838b1f43eb6cf86d65e9a367f4c30edcca672c
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-13 16:56:00 +0300

    Style profile block

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,36 @@
-function Profile() {
+import { faCamera, faPen } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { ProfileCallBackType } from "../types"
+
+interface ProfileProps {
+  handleProfile: ProfileCallBackType
+}
+
+function Profile({ handleProfile }: ProfileProps) {
   return (
-    <div id="profile">Profile</div>
+    <div id="profile">
+      <div id="profile-header">
+        <img src="../assets/images/front-view-smiley-woman-holding-box.jpg" alt="user" />
+        <FontAwesomeIcon icon={faCamera} className="profile-icons" id="camera" />
+      </div>
+
+      <div id="username">
+        <h3>John Doe</h3>
+        <FontAwesomeIcon icon={faPen} className="profile-icons" />
+      </div>
+      <div className="email">
+        <p>johnDoe@gmail.com</p>
+        <FontAwesomeIcon icon={faPen} className="profile-icons" />
+      </div>
+
+      <div className="action-profile-btns">
+        <button>Reset Password</button>
+        <button onClick={handleProfile} >Cancel</button>
+      </div>
+
+
+
+    </div>
   )
 }
 
--- src/components/SidebarLeft.tsx
+++ src/components/SidebarLeft.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
+import { ProfileCallBackType } from "../types"
+import Profile from "./Profile"
+
 const LeftSideBar: React.FC = () => {
+    const [profile, setProfile] = useState<Boolean>(false)
+    const handleProfile: ProfileCallBackType = () => {
+        setProfile(prevState => !prevState)
+    }
     return (
+
         <div id="left-sidebar">
-            <div id="left-sidebar-header">
+            <div id="left-sidebar-header" onClick={handleProfile}>
                 <img src="../assets/images/user.png" alt="profile" />
             </div>
+
+            {profile && <Profile handleProfile={handleProfile} />}
+
             <div id="left-sidebar-body">
                 <button id="add-post-btn">Add Post</button>
             </div>
+
         </div>
+
+
+
     )
 }
 
