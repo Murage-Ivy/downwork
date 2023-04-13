41884ef8d76bc9f53615ef45bf1f814700a67432
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-13 20:52:59 +0300

    Add routing

--- src/components/LandingPageContent.tsx
+++ src/components/LandingPageContent.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function LandingPageContent() {
     return (
@@ -28,7 +29,7 @@ function LandingPageContent() {
                     </div>
                 </div>
                 <div id="landing-page-body-content-btn">
-                    <button className="btn btn-primary">Get Started</button>
+                    <Link to="/login"><button className="btn btn-primary">Get Started</button></Link>
                 </div>
             </div>
 
