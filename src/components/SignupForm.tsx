96b5ad0080863ab9f605045a9f40f65b52725e90
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-20 00:27:25 +0300

    Upload images to cloudniary

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -2,12 +2,17 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const SignUp: React.FC = () => {
+    const cloud_name = "dhpmstfkj"
+    const upload_preset = "p9zcdovn"
+    const api_key = "976857633417912"
+
     type UserType = {
         email: string,
         username: string,
         password: string,
         password_confirmation: string,
-        image: string,
+        image: File | null
+
     }
 
     const [user, setUser] = useState<UserType>({
@@ -15,18 +20,52 @@ const SignUp: React.FC = () => {
         username: "",
         password: "",
         password_confirmation: "",
-        image: "",
+        image: null
+
     })
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target
+        if (name === "image") {
+            setUser({ ...user, [name]: event.target.files![0] })
+            return
+        }
         setUser({ ...user, [name]: value })
+
     }
+    const handleSubmit = (event: React.MouseEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData()
+        const imageFormData = new FormData()
+        formData.append("email", user.email)
+        formData.append("username", user.username)
+        formData.append("password", user.password)
+        formData.append("password_confirmation", user.password_confirmation)
+
+        imageFormData.append("file", user.image!)
+        imageFormData.append("upload_preset", upload_preset)
+        imageFormData.append("cloud_name", cloud_name)
+        imageFormData.append("api_key", api_key)
+
+
+        fetch(`https://api.cloudinary.com/v1_1/dhpmstfkj/image/upload`, {
+            method: "POST",
+            body: imageFormData
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                formData.append("image", data.secure_url)
+            })
+
+    }
+
+
     return (
         <div id="sign-form">
             <div id="sign-form-content">
 
-                <form id="form-signup">
+                <form id="form-signup" onSubmit={handleSubmit}>
                     <h1>SIGN UP</h1>
                     <div className="sign-form-group">
                         <input type="email"
@@ -62,7 +101,7 @@ const SignUp: React.FC = () => {
                             onChange={handleChange} />
                     </div>
                     <div className="sign-form-group">
-                        <input type="file" name="image" id="image" />
+                        <input type="file" name="image" id="image"  onChange={handleChange}/>
                     </div>
                     <div className="sign-form-group">
                         <button id="sign-up-btn" type="submit">Sign Up</button>
