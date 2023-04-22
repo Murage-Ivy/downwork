47594d09e30fefcdc7ad272809c9eefb8e0b1a84
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-22 20:00:47 +0300

    Make the form controlled

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,12 +1,20 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import { LoggedUserType } from "../types"
 
 function LoginForm() {
 
-    const [user, setUser] = useState({
+    const [user, setUser] = useState<LoggedUserType>({
         email: "",
         password: ""
     })
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target
+        setUser({ ...user, [name]: value })
+    }
+
+
     return (
         <div id="login-form">
             <div className="form">
@@ -21,11 +29,23 @@ function LoginForm() {
             <form id="form">
                 <div className="form-group">
 
-                    <input type="email" name="email" id="email" placeholder="email" />
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={user.email}
+                        onChange={handleChange}
+                        placeholder="email" />
                 </div>
                 <div className="form-group">
 
-                    <input type="password" name="password" id="password" placeholder="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={user.password}
+                        onChange={handleChange}
+                        placeholder="password" />
                 </div>
                 <div className="form-group">
                     <button className="btn btn-login">Login</button>
