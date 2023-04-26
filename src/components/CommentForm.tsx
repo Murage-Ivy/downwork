883a1585dbdb429d5bfcba6c6b0e994f84d838c2
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-26 21:23:55 +0300

    Make comment form controlled

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,10 +1,22 @@
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
 
 const CommentForm: React.FC = () => {
+
+    const [comment, setComment] = useState({
+        id: 0,
+        content: "",
+        post_id: 0
+    })
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target
+        setComment({ ...comment, [name]: value })
+    }
     return (
         <div id="comment-form">
-            <input type="text" placeholder="add comment ..." />
+            <input type="text" placeholder="add comment ..." onChange={handleChange} />
             <FontAwesomeIcon icon={faPaperPlane} id="comment-icon" />
         </div>
     )
