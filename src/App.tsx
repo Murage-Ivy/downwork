620b79ef98d7e821e2c92950275120c972efeb1e
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-28 23:30:14 +0300

    Auto log user

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import LoginPage from "./Pages/LoginPage";
 import PostPage from "./Pages/PostPage";
 import SignUpPage from "./Pages/SignUpPage";
 import { SignUpContextProvider } from "./contexts/UserSignupContext";
 import { LoginContextProvider } from "./contexts/UserLoginContext";
+import { useEffect, useState } from "react";
+import { UserType } from "./types";
 
 
 
@@ -12,49 +14,55 @@ import { LoginContextProvider } from "./contexts/UserLoginContext";
 
 const App: React.FC = () => {
 
-  // const [user, setUser] = useState<UserType>({
-  //   id: 0,
-  //   username: "",
-  //   email: "",
-  //   image_url: "",
-  // })
+  const [user, setUser] = useState<UserType>({
+    id: 0,
+    username: "",
+    email: "",
+    image_url: "",
+  })
 
-  // type getUserType = {
-  //   getUser: () => void
+  type getUserType = {
+    getUser: () => void
 
-  // }
+  }
 
-  // const getUser = async () => {
-  //   const response = await fetch('auto_login')
-  //   const data = await response.json()
-  //   if (response.ok) {
-  //     setUser(data)
-  //   }
-  //   else {
-  //     console.log(data.error)
-  //   }
-  // }
+  const getUser = async () => {
+    const response = await fetch('auto_login')
+    const data = await response.json()
+    if (response.ok) {
+      localStorage.setItem("user", data)
+    }
+    else {
+      console.log(data.error)
+    }
+  }
 
-  // const navigate = useNavigate()
 
-  // useEffect(() => {
-  //   getUser()
-  // }, [])
+  useEffect(() => {
+    getUser()
+    const user = JSON.stringify(localStorage.getItem("user"))
+    if (user) {
+      const loggedUser = JSON.parse(user)
+      setUser(loggedUser)
+    }
+  }, [])
 
-  // useEffect(() => {
-  //   if (user && user.id !== 0) {
-  //     navigate('/postpage')
-  //   }
-  // }, [navigate, user])
+  const autoLogin = () => {
+    if (user) {
+      return <Route path="/postpage" element={<PostPage />} />
+    }
+  }
 
 
   return (
     <div className="App">
+
       <Routes>
-        <Route path="/" element={<LandingPage  />} />
+        {user ? <Route path="/postpage" element={<PostPage />} /> : null}
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginContextProvider><LoginPage /></LoginContextProvider>} />
         <Route path="/signup" element={<SignUpContextProvider><SignUpPage /></SignUpContextProvider>} />
-        <Route path="/postpage" element={<PostPage  />} />
+        <Route path="/postpage" element={<PostPage />} />
       </Routes>
 
     </div>
